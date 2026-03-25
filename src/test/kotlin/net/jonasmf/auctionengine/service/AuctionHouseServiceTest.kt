56e2533273a3f26f6051efefbc949e5179956f73
ukon1990 <10864030+ukon1990@users.noreply.github.com>
2026-03-25 22:32:05 +0100

    test(service): Add AuctionHouseServiceTest for getReadyForUpdate

---
+++ src/test/kotlin/net/jonasmf/auctionengine/service/AuctionHouseServiceTest.kt
@@ -0,0 +1,39 @@
+package net.jonasmf.auctionengine.service
+
+import net.jonasmf.auctionengine.constant.Region
+import net.jonasmf.auctionengine.dbo.dynamodb.AuctionHouseDynamo
+import net.jonasmf.auctionengine.repository.dynamodb.AuctionHouseDynamoRepository
+import org.junit.Assert
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class AuctionHouseServiceTest(
+    val repository: AuctionHouseDynamoRepository,
+    val auctionHouseService: AuctionHouseService,
+) {
+    val auctionHouses = listOf<AuctionHouseDynamo>(
+        AuctionHouseDynamo(
+            id = 1,
+            autoUpdate = true,
+            region = Region.Europe,
+        ),
+        AuctionHouseDynamo(
+            id = 2,
+            autoUpdate = true,
+            region = Region.NorthAmerica,
+        ),
+    )
+
+    fun beforeAll() {
+        auctionHouses.forEach {repository.save(it) }
+    }
+
+    @Nested
+    inner class GetReadyForUpdate() {
+        @Test
+        fun canGetAuctionHouses() {
+            val result = auctionHouseService.getReadyForUpdate(Region.Europe)
+            Assert.assertEquals(auctionHouses.size, 1)
+        }
+    }
+}
