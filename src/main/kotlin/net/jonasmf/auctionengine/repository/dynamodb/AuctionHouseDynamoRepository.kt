f0d06bc09dcc6b98a9016261cf6549a4070bc04b
ukon1990 <10864030+ukon1990@users.noreply.github.com>
2026-03-25 22:32:04 +0100

    feat(repository): Add findAllByRegion to AuctionHouseDynamoRepository

--- src/main/kotlin/net/jonasmf/auctionengine/repository/dynamodb/AuctionHouseDynamoRepository.kt
+++ src/main/kotlin/net/jonasmf/auctionengine/repository/dynamodb/AuctionHouseDynamoRepository.kt
@@ -1,15 +1,21 @@
 package net.jonasmf.auctionengine.repository.dynamodb
 
 import io.awspring.cloud.dynamodb.DynamoDbOperations
+import net.jonasmf.auctionengine.constant.Region
 import net.jonasmf.auctionengine.dbo.dynamodb.AuctionHouseDynamo
 import org.springframework.stereotype.Repository
+import software.amazon.awssdk.enhanced.dynamodb.Expression
 import software.amazon.awssdk.enhanced.dynamodb.Key
-import java.util.Optional
+import software.amazon.awssdk.enhanced.dynamodb.model.QueryEnhancedRequest
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue
+import java.util.*
 
 const val AUCTION_HOUSE_TABLE_NAME = "wah_auction_houses"
 
 interface AuctionHouseDynamoRepository {
     fun findById(id: Int?): Optional<AuctionHouseDynamo>
+
+    fun findAllByRegion(region: Region): List<AuctionHouseDynamo>
 
     fun save(auctionHouse: AuctionHouseDynamo): AuctionHouseDynamo
 }
@@ -33,6 +39,28 @@ class DynamoDbAuctionHouseRepository(
             )
 
         return Optional.ofNullable(entity)
+    }
+
+    override fun findAllByRegion(region: Region): List<AuctionHouseDynamo> {
+
+        val query = QueryEnhancedRequest
+            .builder()
+            .filterExpression(
+                Expression
+                    .builder()
+                    .expression("region = :region")
+                    .putExpressionValue(":region", AttributeValue.builder().s(region.name).build())
+                    .build(),
+            )
+            .build()
+
+        val entity =
+            dynamoDbOperations.query<AuctionHouseDynamo>(
+                query,
+                AuctionHouseDynamo::class.java,
+            )
+
+        return entity ?: emptyList<AuctionHouseDynamo>()
     }
 
     override fun save(auctionHouse: AuctionHouseDynamo): AuctionHouseDynamo {
