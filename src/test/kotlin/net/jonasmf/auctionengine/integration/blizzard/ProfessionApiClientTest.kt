0e6353555f24a934b6c938879eb34f2edeb7762a
Jonas <10864030+ukon1990@users.noreply.github.com>
2026-04-09 19:30:39 +0200

    forgot to add this

--- src/test/kotlin/net/jonasmf/auctionengine/integration/blizzard/ProfessionApiClientTest.kt
+++ src/test/kotlin/net/jonasmf/auctionengine/integration/blizzard/ProfessionApiClientTest.kt
@@ -6,16 +6,18 @@ import org.junit.jupiter.api.Test
 class ProfessionApiClientTest {
     @Test
     fun getAll() {
+        // TODO
     }
 
     @Test
     fun getById() {
+        // TODO
     }
 
-    private fun professionIndexBody(): String = loadFixture("blizzard/profession/index-response.json")
+    private fun professionIndexBody(): String = loadFixture(this, "blizzard/profession/index-response.json")
 
-    private fun professionById(id: Int): String = loadFixture("blizzard/profesion/details/$id-response.json")
+    private fun professionById(id: Int): String = loadFixture(this, "blizzard/profesion/details/$id-response.json")
 
     private fun professionSkillTierById(id: Int): String =
-        loadFixture("blizzard/profesion/skill-tier/$id-response.json")
+        loadFixture(this, "blizzard/profesion/skill-tier/$id-response.json")
 }
