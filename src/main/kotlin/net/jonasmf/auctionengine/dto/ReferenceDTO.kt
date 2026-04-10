4cd24aaee9465d6b4a0fe0327ae5beee21f42306
ukon1990 <10864030+ukon1990@users.noreply.github.com>
2026-04-10 16:36:39 +0200

    feat(dto): Introduce ReferenceDTO for common entity references

---
+++ src/main/kotlin/net/jonasmf/auctionengine/dto/ReferenceDTO.kt
@@ -0,0 +1,11 @@
+package net.jonasmf.auctionengine.dto
+
+/**
+ * Data class for repeating id, name, key types.
+ * Used in the "parent" objects, and references the main object of the recipe for example
+ */
+data class ReferenceDTO(
+    val id: Int,
+    val name: LocaleDTO,
+    val key: Href,
+)
