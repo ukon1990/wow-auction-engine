2886b3ecd67477f144ab8ed7bf6683cbc74c24ba
ukon1990 <10864030+ukon1990@users.noreply.github.com>
2026-04-10 16:36:40 +0200

    feat(blizzard): Implement getAll professions endpoint

--- src/main/kotlin/net/jonasmf/auctionengine/integration/blizzard/ProfessionApiClient.kt
+++ src/main/kotlin/net/jonasmf/auctionengine/integration/blizzard/ProfessionApiClient.kt
@@ -1,12 +1,31 @@
 package net.jonasmf.auctionengine.integration.blizzard
 
+import net.jonasmf.auctionengine.constant.Region
+import net.jonasmf.auctionengine.dto.profession.ProfessionIndexDTO
+import org.springframework.web.reactive.function.client.bodyToMono
 import reactor.core.publisher.Mono
 
 class ProfessionApiClient(
     private val blizzardApiSupport: BlizzardApiSupport,
 ) {
     // profession index
-    fun getAll() {}
+    fun getAll(): ProfessionIndexDTO {
+        // TODO: Cleanup, not sure if there is a point passing in region. blizzard api support has this info
+        val region = Region.NorthAmerica
+        val uri =
+            blizzardApiSupport.buildRegionalUri(
+                region = region,
+                path = "/api/profession/all",
+                namespace = blizzardApiSupport.dynamicNamespaceForRegion(region).value,
+            )
+        return blizzardApiSupport
+            .webClient()
+            .get()
+            .uri(uri)
+            .retrieve()
+            .bodyToMono<ProfessionIndexDTO>()
+            .block()!!
+    }
 
     fun getById(id: Int): Mono<Any> {}
 }
