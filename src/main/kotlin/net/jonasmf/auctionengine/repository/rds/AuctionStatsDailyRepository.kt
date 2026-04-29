db9ae1ac06b443cd5fd4b0f1fafc4ffff5c94490
jonas <10864030+ukon1990@users.noreply.github.com>
2026-04-29 05:39:20 +0200

    refactor(daily-stats): Rename DailyAuctionStats DBO and update associated repositories

---
+++ src/main/kotlin/net/jonasmf/auctionengine/repository/rds/AuctionStatsDailyRepository.kt
@@ -0,0 +1,7 @@
+package net.jonasmf.auctionengine.repository.rds
+
+import org.springframework.stereotype.Repository
+
+@Repository
+class AuctionStatsDailyRepository {
+}
--- src/main/kotlin/net/jonasmf/auctionengine/repository/rds/DailyAuctionStatsRepository.kt
+++ src/main/kotlin/net/jonasmf/auctionengine/repository/rds/DailyAuctionStatsRepository.kt
@@ -1,8 +1,8 @@
 package net.jonasmf.auctionengine.repository.rds
 
-import net.jonasmf.auctionengine.dbo.rds.auction.DailyAuctionStats
+import net.jonasmf.auctionengine.dbo.rds.auction.AuctionStatsDaily
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface DailyAuctionStatsRepository : JpaRepository<DailyAuctionStats, Long>
+interface DailyAuctionStatsRepository : JpaRepository<AuctionStatsDaily, Long>
