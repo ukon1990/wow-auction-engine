853154af25e9d0e5a330dcf306dc7e368ac4ac48
Jonas <10864030+ukon1990@users.noreply.github.com>
2026-05-19 16:13:43 +0200

    Chore: Add Storybook i18n support

--- frontend/.storybook/preview.ts
+++ frontend/.storybook/preview.ts
@@ -1,3 +1,4 @@
+import '@angular/localize/init';
 import { applicationConfig } from '@storybook/angular';
 import { provideRouter } from '@angular/router';
 import type { Preview } from '@storybook/angular';
