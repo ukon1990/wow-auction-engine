3ee0d8a5925d42e6d345a9d55cf2832127d3d51a
Jonas <10864030+ukon1990@users.noreply.github.com>
2026-05-22 17:44:58 +0200

    fix(build): Add missing comma in `app.routes.server.ts`

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -12,6 +12,6 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: 'admin/**',
     renderMode: RenderMode.Server,
-  }
+  },
   // doing SSR for the auctions page was a bit memory intensive it seems, so we're going to do client-side rendering for now
 ];
