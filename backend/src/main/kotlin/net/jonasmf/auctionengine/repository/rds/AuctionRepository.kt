a6ff99fbe6cd0faa2d674d61bc068129939b2147
Jonas <10864030+ukon1990@users.noreply.github.com>
2026-06-17 21:49:42 +0200

    refactor(repo): Update AuctionRepository to use String ID

--- backend/src/main/kotlin/net/jonasmf/auctionengine/repository/rds/AuctionRepository.kt
+++ backend/src/main/kotlin/net/jonasmf/auctionengine/repository/rds/AuctionRepository.kt
@@ -5,4 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface AuctionRepository : JpaRepository<Auction, Long>
+interface AuctionRepository : JpaRepository<Auction, String>
