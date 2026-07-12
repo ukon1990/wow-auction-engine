0f8b4ff039f63e05a616634350d3115aa7fafd16
Jonas <10864030+ukon1990@users.noreply.github.com>
2026-07-12 20:17:33 +0200

    work

--- backend/src/test/kotlin/net/jonasmf/auctionengine/service/DeletedAuctionCleanupServiceTest.kt
+++ backend/src/test/kotlin/net/jonasmf/auctionengine/service/DeletedAuctionCleanupServiceTest.kt
@@ -1,8 +1,11 @@
 package net.jonasmf.auctionengine.service
 
 import net.jonasmf.auctionengine.config.IntegrationTestBase
+import net.jonasmf.auctionengine.constant.Region
 import net.jonasmf.auctionengine.dbo.rds.realm.AuctionHouse
 import net.jonasmf.auctionengine.dbo.rds.realm.ConnectedRealm
+import net.jonasmf.auctionengine.dbo.rds.realm.Realm
+import net.jonasmf.auctionengine.dbo.rds.realm.RegionDBO
 import net.jonasmf.auctionengine.repository.rds.AuctionRepository
 import net.jonasmf.auctionengine.repository.rds.AuctionStatsHourlyJDBCRepository
 import org.junit.jupiter.api.BeforeEach
@@ -12,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.jdbc.core.JdbcTemplate
 import java.time.Duration
 import java.time.Instant
+import kotlin.test.assertEquals
 
 class DeletedAuctionCleanupServiceTest : IntegrationTestBase() {
     @Autowired
@@ -31,6 +35,9 @@ class DeletedAuctionCleanupServiceTest : IntegrationTestBase() {
 
     @Autowired
     lateinit var jdbcTemplate: JdbcTemplate
+
+    @Autowired
+    lateinit var blizzardAuctionService: BlizzardAuctionService
 
     @BeforeEach
     fun beforeEach() {
@@ -52,27 +59,50 @@ class DeletedAuctionCleanupServiceTest : IntegrationTestBase() {
                 id = 2,
                 auctionHouse =
                     AuctionHouse(
+                        nextUpdate = yesterday,
+                        lastModified = yesterday,
                         lastAuctionPriceDeleteEvent = yesterday,
                         lastHistoryDeleteEvent = yesterday,
                         lastHistoryDeleteEventDaily = yesterday,
+                        region = Region.Europe,
+                    ),
+                realms =
+                    mutableListOf(
+                        Realm(
+                            id = 1,
+                            region =
+                                RegionDBO(
+                                    1,
+                                    name = "Europe",
+                                    type = Region.Europe,
+                                ),
+                            name = "Test realm",
+                            category = TODO(),
+                            locale = TODO(),
+                            timezone = TODO(),
+                            gameBuild = TODO(),
+                            slug = TODO(),
+                        ),
                     ),
             ),
         )
     }
 
-    @Nested
-    sealed class CleanupOfDailyAuctionStatistics {
-        @Test
-        fun `Should successfully delete daily stats and update only for old data`() {
-            jdbcTemplate.update(
-                """
-                INSERT INTO auction (id, connected_realm_id, update_history_id,)
-                VALUES (
-
-                )
-                """.trimIndent(),
+    @Test
+    fun `Should successfully delete daily stats and update only for old data`() {
+        /*jdbcTemplate.update(
+            """
+            INSERT INTO auction (id, connected_realm_id, update_history_id,)
+            VALUES (
+                'asdsad'
             )
-            val result = auctionStatsDailyRepository.getForConnectedRealm(2)
-        }
+            """.trimIndent(),
+        )
+        val result = auctionStatsDailyRepository.getForConnectedRealm(2)
+         */
+
+        val ah = auctionHouseService.getReadyForUpdate(Region.Europe)
+        blizzardAuctionService.updateAuctionHouses(Region.Europe, ah)
+        assertEquals(1, 2)
     }
 }
